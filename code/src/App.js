ecd4f5c8cb50140b11db3193afbf8d02283304e0
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-12-05 14:13:57 +0100

    omg, it works, i can post- me happy

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
-import {HappyThought} from './components/HappyThought'
-import {HappyForm} from './components/HappyForm'
+import { HappyThought } from "./components/HappyThought"
+import { HappyForm } from "./components/HappyForm"
 
 const url = "https://technigo-thoughts.herokuapp.com/"
 
@@ -9,26 +9,26 @@ export const App = () => {
   const [postedMessage, setPostedMessage] = useState("")
 
   useEffect(() => {
-    fetch("https://technigo-thoughts.herokuapp.com/")
+    fetch(url)
       .then(res => res.json())
       .then(json => setThoughts(json))
   }, [postedMessage])
 
   const handleFormSubmit = message => {
-    fetch("https://technigo-thoughts.herokuapp.com/", {
-      method: "POST", 
-      body: JSON.stringify({ message }), 
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ message }),
       headers: { "Content-Type": "application/json" }
     })
-    .then(() => setPostedMessage(message))
-    .catch(err => console.log("error:", err))
+      .then(() => setPostedMessage(message))
+      .catch(err => console.log("error:", err))
   }
 
   return (
     <main>
       <HappyForm onFormSubmit={handleFormSubmit} />
       {thoughts.map(thought => (
-       <HappyThought key={thought._id} thought={thought} /> 
+        <HappyThought key={thought._id} thought={thought} />
       ))}
     </main>
   )
--- code/src/components/HappyForm.js
+++ code/src/components/HappyForm.js
@@ -1,32 +1,32 @@
+ 
 import React, { useState } from "react"
-import 'components/happyForm.css'
-
+import "./happyForm.css"
 
 export const HappyForm = props => {
-    const[message, setMessage] = useState("")
+  const [message, setMessage] = useState("")
 
-    const handleSubmit = event => {
-      event.preventDefautl()
-      props.onFormSubmit(message)  
-    }
+  const handleSubmit = event => {
+    event.preventDefault()
+    props.onFormSubmit(message) // this onFormSubmit comes as a props from App.js
+  }
 
-     return (
-        <form className='happy-form'>
-            <h3>Post a happy thought!</h3>
-            <textarea 
-                rows='3'
-                onChange={event => setMessage(event.target.value)}
-            ></textarea>
-            <div className='form-footer'>
-                <button 
-                    type='submit' 
-                    onClick={handleSubmit}
-                    disabled={message.length < 6 || message.length > 140 ? true : false}
-                >
-                    Send a happy thought
-                </button>
-                <p>{message.length} / 140</p>
-            </div>
-        </form>
-    )
+  return (
+    <form className='happy-form'>
+      <h3>Post a happy thought!</h3>
+      <textarea
+        rows='3'
+        onChange={event => setMessage(event.target.value)}
+      ></textarea>
+      <div className='form-footer'>
+        <button
+          type='submit'
+          onClick={handleSubmit}
+          disabled={message.length < 6 || message.length > 140 ? true : false}
+        >
+          Send a happy thought
+        </button>
+        <p>{message.length} / 140</p>
+      </div>
+    </form>
+  )
 }
